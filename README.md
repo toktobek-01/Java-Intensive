766ee3e51dbe466ee389835672dc481c1ab22e33
toktobek-01 <toktobek.d@gmail.com>
2025-03-30 21:44:26 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Java-Intensive
+# Java-Intensive
--- src/arrays.java
+++ src/arrays.java
@@ -1,16 +1,17 @@
 public class arrays {
     // for {int i= 3, i <5, i++; }
     public static void main(String[] args) {
-        String[] family = {
-                "John 1980", "Alex 1990", "Smith 1789", "James 1987"
+        String[] months = {
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
         };
-
-        int i;
-        for (i = 0; i < family.length; i++) {
-
-            System.out.println(family[i]);
+        //for (int i = 0; i < months.length; i++) ;
+        //System.out.println(months[i]);
+        for (int i = 0; i < months.length; i++) {
+            System.out.println(months[i]);
         }
     }
+}
     //public static void main(String[] args) {
        // int a, b, c, d, e;
         //int[] array = new int[100];    //int[]array = new int[]{10, 20, 30};
@@ -18,7 +19,7 @@ public class arrays {
        // array[1] = 120;
 
         //System.out.println(array[1]);
-    }
+
 
 
 
