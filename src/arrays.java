6743fe6e7c3711c6daadc439f809d2e506255525
toktobek-01 <toktobek.d@gmail.com>
2025-03-30 21:37:03 -0500

    Names Arry

---
+++ src/arrays.java
@@ -0,0 +1,25 @@
+public class arrays {
+    // for {int i= 3, i <5, i++; }
+    public static void main(String[] args) {
+        String[] family = {
+                "John 1980", "Alex 1990", "Smith 1789", "James 1987"
+        };
+
+        int i;
+        for (i = 0; i < family.length; i++) {
+
+            System.out.println(family[i]);
+        }
+    }
+    //public static void main(String[] args) {
+       // int a, b, c, d, e;
+        //int[] array = new int[100];    //int[]array = new int[]{10, 20, 30};
+      //  array[0] = 100;
+       // array[1] = 120;
+
+        //System.out.println(array[1]);
+    }
+
+
+
+
