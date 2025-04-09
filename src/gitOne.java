dba3bc32855e6579a489c71435a8eb00d68747c5
toktobek-01 <toktobek.d@gmail.com>
2025-04-09 09:51:42 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ src/gitOne.java
@@ -0,0 +1,11 @@
+public class gitOne {
+    public static void main(String[] args) {
+
+        int weeksInYear = 52;
+
+        // Loop through each week
+        for (int i = 1; i <= weeksInYear; i++) {
+            System.out.println("Week " + i);
+        }
+    }
+}
