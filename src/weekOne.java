73c08d888eba82041656247b99c9b503ec23ee97
toktobek-01 <toktobek.d@gmail.com>
2025-04-09 20:43:30 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ src/weekOne.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class weekOne {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+       // String name = sc.next();
+        //System.out.println("Hello " + name);
+
+        //Scanner scan = new Scanner(System.in);
+       // String myString = scan.next();
+        //int myInt = scan.nextInt();
+
+       // System.out.println("myString is:" + myString);
+       // System.out.println("myInt is:" + 5);
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        int old = sc.nextInt();
+        int oldest = sc.nextInt();
+
+        System.out.println(age);
+        System.out.println(old);
+        System.out.println(oldest);
+    }
+    }
+
