35e3f8b1ebd364ac8729dd7c7f838957bcbf80d8
toktobek-01 <toktobek.d@gmail.com>
2025-04-11 18:19:22 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ src/greetSomeOne.java
@@ -0,0 +1,10 @@
+public class greetSomeOne {
+    public static void greet (String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet("Taalai");
+        greet("Aida");
+    }
+}
