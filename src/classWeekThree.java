496daada00a2bfba5f274324eb0e76b0a6fd8c01
toktobek-01 <toktobek.d@gmail.com>
2025-04-20 14:01:03 -0600

    Merge remote-tracking branch 'origin/main'

--- src/classWeekThree.java
+++ src/classWeekThree.java
@@ -3,6 +3,8 @@ import javax.crypto.spec.PSource;
 public class classWeekThree {
     public static void main(String[] args) {
         String test = "Here is a hero!";
+        test += " t";
+        System.out.println(test);
         char[] c = new char[]{'s', 't', 'r', 'i', 'n', 'g'};
         for (int i = 0; i < c.length; ++i) {
             System.out.println(c[i]);
