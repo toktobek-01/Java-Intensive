0caa5b2ed7be65c75a98a5fba2c110d72275c971
toktobek-01 <toktobek.d@gmail.com>
2025-04-20 16:35:57 -0600

    Merge remote-tracking branch 'origin/main'

---
+++ src/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(sum(3.4, 4.3));
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static double sum(double a, double b) {
+        return a * b;
+    }
+}
