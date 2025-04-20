a85438cdc80e4f58bbc89e855e1995d651721a97
toktobek-01 <toktobek.d@gmail.com>
2025-04-20 17:19:36 -0600

    Merge remote-tracking branch 'origin/main'

---
+++ src/recursion.java
@@ -0,0 +1,13 @@
+public class recursion {
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+}
