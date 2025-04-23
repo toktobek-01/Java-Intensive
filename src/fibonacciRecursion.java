c9fb1c04f683286ebf76c10a7016f022fb2a0c87
toktobek-01 <toktobek.d@gmail.com>
2025-04-23 09:26:15 -0500

    Merge remote-tracking branch 'origin/main'

--- src/fibonacciRecursion.java
+++ src/fibonacciRecursion.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
-public class fibonacciRecursion
-
- {
+public class fibonacciRecursion {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
 
-        System.out.print("Enter the number of terms to print: ");
+        System.out.print("Enter the position (n) of the Fibonacci number: ");
         while (!sc.hasNextInt()) {
             System.out.println("Invalid input. Please enter an integer.");
             sc.next();
@@ -15,10 +13,11 @@ public class fibonacciRecursion
 
         n = sc.nextInt();
 
-        System.out.println("Fibonacci sequence up to " + n + " terms:");
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(fibonacci(i) + " ");
+        if (n < 0) {
+            System.out.println("Please enter a non-negative number.");
+        } else {
+            int result = fibonacci(n);
+            System.out.println("The " + n + getOrdinalSuffix(n) + " Fibonacci number is " + result);
         }
 
         sc.close();
@@ -29,5 +28,21 @@ public class fibonacciRecursion
         if (n == 1) return 1;
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
+    // Just for fun: This method adds "st", "nd", "rd", "th"
+    public static String getOrdinalSuffix(int n) {
+        if (n % 100 >= 11 && n % 100 <= 13) return "th";
+        switch (n % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
 }
+
 
