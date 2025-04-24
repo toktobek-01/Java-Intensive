fc119b7635d768e995cfb8045be853dc8058a876
toktobek-01 <toktobek.d@gmail.com>
2025-04-24 08:41:51 -0500

    Merge remote-tracking branch 'origin/main'

--- src/recursion.java
+++ src/recursion.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class recursion {
-    public static int factorial(int n) {
-        if (n == 1) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = factorial(num);
+        System.out.println("Factorial of " + num + " is: " + result);
+
+    }
+    public static int factorial(int num) {
+        if (num == 1) {
             return 1;
         } else {
-            return factorial(n - 1) * n;
+            return factorial(num - 1) * num;
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(factorial(4));
     }
 }
